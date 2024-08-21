312be80937e18402372ba3e3357d6eb48bc95742
rohitsmart <rohitsmart95@gmail.com>
2024-08-21 15:00:04 +0530

    upadte with endpoint get employee

--- src/assests/Endpoints.js
+++ src/assests/Endpoints.js
@@ -3,7 +3,8 @@ const BASE_URL = "http://localhost:8080/api/";
 export const ENDPOINTS = {
     LOGIN: `${BASE_URL}public/login`,
     USER_EMPLOYEE_DETAILS: `${BASE_URL}protected/management/user-employee-details`,
-    ADD_EMPLOYEE:`${BASE_URL}protected/management/save`
+    ADD_EMPLOYEE:`${BASE_URL}protected/management/save`,
+    GET_EMPLOYEES:`${BASE_URL}protected/management/employees`
 };
 
 export default ENDPOINTS;
